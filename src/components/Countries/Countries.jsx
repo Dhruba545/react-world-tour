fbf600ac53b9f4f8526602f2f6be719e0aa7ac85
dhruba371 <dhrubabiswaslogin371@gmail.com>
2024-03-15 16:22:42 +0600

    second commit

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,9 +1,11 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import Country from "../Country/Country";
+import './Countries.css';
 
  const Countries = () => {
-const [countries , setCountries] = useState([])
+const [countries , setCountries] = useState([]);
+const [visitedCountried , setVisitedCountries] = useState([]);
         
       useEffect(() =>{
           fetch('https://restcountries.com/v3.1/all')
@@ -11,13 +13,22 @@ const [countries , setCountries] = useState([])
           .then(data => setCountries(data));
       },[])
 
+         const handleVisitedCountry = country =>{
+            console.log('add this country to your vivited coutry');
+            console.log(country); 
+         }
 
     return (
         <div>
             <h3>Countries:{countries.length}</h3>
-            {
-                countries.map(country =><Country key={country.cca3} country={country}></Country>)
+            <div><h5>Visited countries</h5>
+            <ul>
+                </ul> </div>
+           <div  className="country-container">
+           {
+                countries.map(country =><Country btnProps = {handleVisitedCountry} key={country.cca3} country={country}></Country>)
             }
+           </div>
         </div>
     );
 };
--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,13 +1,27 @@
+import { useState } from 'react';
 import './country.css';
-const Country = ({country}) => {
-    console.log(country);
-    const {name ,flags , population , area} = country;
+const Country = ({country , btnProps}) =>{
+    const {name ,flags , population , area ,cca3} = country;
+
+    const [visited , setVisited] = useState(false);
+    const handleVisited = () =>{
+        setVisited(!visited);
+    }
+
+    // const passWithParams =() =>  btnProps(country);
+    
+
     return (
-        <div className='country'>
+        <div className={`country ${visited && 'visited'}`}>
             <h3>Name:{name?.common}</h3>
             <img src={flags.png}></img>
             <p>Population: {population}</p>
             <p>Area:{area}</p>
+            <p><small>CCS3:{cca3}</small></p>
+            <button onClick={handleVisited}>{visited ? 'Visited' : 'Going'}</button>
+            {visited && 'I have visited this country'}<br></br>
+            <button onClick={() =>  btnProps(country)}>Mark as visited</button>
+        
         </div>
     );
 };
